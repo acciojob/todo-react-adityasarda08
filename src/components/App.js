492646dfe13bed02c93d6947202b4a5f51ebea26
aditya <adityasarda08.com>
2026-03-06 09:53:23 +0530

    updated code

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,42 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import Todo from "./Todo";
 
-const App = () => {
+function App() {
+
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = () => {
+    if (task.trim() === "") return;
+
+    setTodos([...todos, task]);
+    setTask("");
+  };
+
+  const deleteTodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="App">
+
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        placeholder="Enter todo"
+      />
+
+      <button onClick={addTodo}>Add Todo</button>
+
+      <Todo todos={todos} deleteTodo={deleteTodo} />
+
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
